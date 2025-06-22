f8b966b961a3056de453b181f501baef41d5bc36
Octaver <Octaver2131@outlook.com>
2025-06-22 13:26:10 +0800

    页面开发

---
+++ src/pages/Index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+zy
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Team.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+tm
+</template>
+
+<style scoped>
+
+</style>
