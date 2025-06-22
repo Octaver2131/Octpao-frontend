782cbc4a29c26958d180a12dde0c89db0e9ba033
Octaver <Octaver2131@outlook.com>
2025-06-22 13:25:11 +0800

    添加组件

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
-import { Button } from 'vant';
+import {Button, Icon, NavBar, Tabbar, TabbarItem} from 'vant';
 
 const app = createApp(App);
 app.use(Button);
+app.use(NavBar);
+app.use(Icon);
+app.use(Tabbar);
+app.use(TabbarItem)
 app.mount('#app');
