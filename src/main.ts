38aa8d5867b58396871b9a39388da037dc64cfa5
Octaver <Octaver2131@outlook.com>
2025-06-25 19:36:33 +0800

    定义路由

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import {Button, Icon, NavBar, Tabbar, TabbarItem} from 'vant';
 import * as VueRouter from 'vue-router'
-import Index from "./pages/Index.vue";
-import Team from "./pages/Team.vue";
+import routes from "./config/route.ts";
 
 const app = createApp(App);
 app.use(Button);
@@ -11,11 +10,6 @@ app.use(NavBar);
 app.use(Icon);
 app.use(Tabbar);
 app.use(TabbarItem)
-
-const routes = [
-    { path: '/', component: Index },
-    { path: '/team', component: Team },
-]
 
 const router = VueRouter.createRouter({
     history: VueRouter.createWebHistory(),
