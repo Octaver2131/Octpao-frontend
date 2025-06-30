dfb1d3764a60db566c13dc42622cb2f8a578c9bd
Octaver <Octaver2131@outlook.com>
2025-06-30 19:52:55 +0800

    添加路由

--- src/config/route.ts
+++ src/config/route.ts
@@ -2,12 +2,14 @@ import Index from "../pages/Index.vue";
 import Team from "../pages/Team.vue";
 import User from "../pages/User.vue";
 import SearchPage from "../pages/SearchPage.vue";
+import EditUserPage from "../pages/EditUserPage.vue";
 
 const routes = [
     { path: '/', component: Index },
     { path: '/team', component: Team },
     { path: '/user', component: User },
-    { path: '/search', component: SearchPage }
+    { path: '/search', component: SearchPage },
+    { path: '/user/edit', component: EditUserPage}
 ]
 
 export default routes;
