9a9ea695c1568cf596d9982410b3fba187189b55
Octaver <Octaver2131@outlook.com>
2025-07-04 17:11:02 +0800

    主页完善

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,43 @@
-<script setup lang="ts">
+<template>
+  <user-card-list :user-list="userList" />
+  <van-empty v-if="!userList || userList.length < 1" description="数据为空"/>
+</template>
+
+<script setup>
+import {onMounted, ref} from "vue";
+import { useRoute } from "vue-router";
+import myAxios from "../plugins/myAxios.ts";
+import UserCardList from "../components/UserCardList.vue";
+
+const route = useRoute();
+const { tags } = route.query;
+const userList = ref([]);
+
+onMounted(async () => {
+  const userListData = await myAxios.get('/user/recommend', {
+    withCredentials: false,
+    params: {
+
+    },
+  })
+      .then(function (response) {
+        console.log('/user/recommend', response);
+        return response?.data
+      })
+      .catch(function (error) {
+        console.error('/user/recommend', error);
+      })
+  if (userListData) {
+    userListData.forEach(user => {
+      if (user.tags) {
+        user.tags = JSON.parse(user.tags);
+      }
+    })
+    userList.value = userListData;
+  }
+})
 
 </script>
-
-<template>
-zy
-</template>
 
 <style scoped>
 
