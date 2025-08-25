f08531b68eb8228b722748091eefa4d2d41f2ce0
Octaver <Octaver2131@outlook.com>
2025-08-26 05:55:44 +0800

    创建队伍页面

---
+++ src/pages/UserTeamCreatePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="teamPage">
+    <van-search v-model="searchText" placeholder="创建队伍" @search="onSearch" />
+    <van-button type="primary" @click="doJoinTeam">加入队伍</van-button>
+    <team-card-list :teamList="teamList" />
+    <van-empty v-if="teamList?.length < 1" description="数据为空" />
+  </div>
+</template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+import TeamCardList from "../components/TeamCardList.vue";
+import {onMounted, ref} from "vue";
+import myAxios from "../plugins/myAxios.js";
+
+const router = useRouter();
+const searchText = ref('');
+
+const doJoinTeam = () => {
+  router.push({
+    path: '/team/add'
+  })
+}
+
+const teamList = ref([]);
+
+const listTeam = async (val = '') => {
+  const res = await myAxios.get('/team/list/my/create', {
+    params: {
+      searchText: val,
+      pageNum: 1,
+    }
+  });
+  if (res?.code === 0) {
+    teamList.value = res.data;
+  } else {
+    alert("获取队伍列表失败，请刷新重试");
+  }
+}
+
+// 页面加载时只触发一次
+onMounted(() => {
+  listTeam();
+})
+
+const onSearch = (val) => {
+  listTeam(val);
+}
+
+</script>
+
+<style scoped>
+
+</style>
